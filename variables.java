20079a6d84f5e2e5c152543e6667b6aef0637be4
Kotresh KV <kotreshkotresh1562@example.com>
2025-02-23 12:58:20 +0530

    int

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+
+public static void main(String[] args) {
+    System.out.println("The sum of these number is: ");
+    int num1 = 9;
+    int num2 = 15;
+    int num3 = 34;
+    int sum = num1 + num2 + num3;
+    System.out.println(sum);
+}
+}
