1be30b8b41c37604ea3eac143af3802cedd4438f
Kotresh KV <kotreshkotresh1562@example.com>
2025-02-23 19:55:55 +0530

    literals

---
+++ literals.java
@@ -0,0 +1,15 @@
+public class literals {
+    public static void main(String[] args) {
+     byte age = 50;
+     int age2 = 34;
+     short age3 = 59;
+     long ageDino = 5666666666666666L;
+     char ch = 'K';
+     float f1 = 5.6f;
+     double d1 = 4.45;
+     boolean b = true;
+     System.out.println(ageDino);
+     String str = "Kotresh";
+     System.out.println(str);  
+ }
+}
