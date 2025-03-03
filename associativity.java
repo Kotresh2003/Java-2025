b9aee05dbdaf2e400b2b6b8e876f97c5dabeea7c
Kotresh KV <kotreshkotresh1562@example.com>
2025-03-04 00:57:36 +0530

    associativity

---
+++ associativity.java
@@ -0,0 +1,20 @@
+public class associativity {
+    public static void main(String[] args) {
+        int a = 6*5-38/2;  // L TO R
+        int b = 60/4-38*2; // R TO L
+        System.out.println(a);
+        System.out.println(b); 
+
+        quick quiz
+        int x = 20;
+        int y =37;
+        int k = x * y/2;
+        System.out.println(k);
+
+        int a = 7;
+        int b = 9;
+        int c = 4;
+        int k = b*b - (4*a*c)/(2*a);
+        System.out.println(k);
+        }
+}
