466f666aec813e212cd44cdc2419062421d50adb
Kotresh KV <kotreshkotresh1562@example.com>
2025-03-04 02:08:33 +0530

    increment and decrement

---
+++ decrement.java
@@ -0,0 +1,21 @@
+public class data {
+    public static void main (String[] args) {
+    byte x = 5;
+    int y = 45;
+    short z = 5;
+    int a = x + z;    
+    float b = 6.56f + x;
+    System.out.println(a);
+
+    //increment and decrement operators
+    int i = 34;
+    System.out.println(i++);
+    System.out.println(i);
+    System.out.println(++i);
+    System.out.println(i);
+    int y = 7;
+    System.out.println((++y)*8);
+    char ch = 'd';
+    System.out.println(++ch);
+    }
+}
