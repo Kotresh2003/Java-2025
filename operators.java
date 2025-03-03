a634efb4b31436a133112a51ada7fa28ea4b12fc
Kotresh KV <kotreshkotresh1562@example.com>
2025-03-04 00:27:40 +0530

    operator arithmetic assignment comparision logical bitwise

---
+++ operators.java
@@ -0,0 +1,14 @@
+public class operators {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 34 % a; //module opertor
+        int b = 9;
+        b *= 3; // assignment operator
+
+        System.out.println(b);
+        System.out.println(64<6); //comparision operator
+        System.out.println(65<68 && 65<69); //logical operator
+        System.out.println(65<10 || 65<10);  // logical operator
+        System.out.println(5&6); //bitwise operator    
+    }
+}
