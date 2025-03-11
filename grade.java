9294bdc52730300d7c9057f9550a3f3a33f3e1b3
Kotresh KV <kotreshkotresh1562@example.com>
2025-03-12 00:09:23 +0530

    grade

---
+++ grade.java
@@ -0,0 +1,11 @@
+public class grade {
+    public static void main(String[] args) {
+        char grade = 'A';
+        grade = (char)(grade + 8);
+        System.out.println(grade);
+
+        // decrpting the grade
+        grade = (char)(grade - 8);
+        System.out.println(grade);
+    }
+}
